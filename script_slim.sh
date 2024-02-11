6650b6f4536d175467ee3914f6d340414bf63862
Exodo2009 <135548519+Exodo2009@users.noreply.github.com>
2024-02-11 14:55:53 -0600

    Create script_slim.sh
    This script is a slim version of the original one without Stephan Raabe's dotfiles.

---
+++ script_slim.sh
@@ -0,0 +1,14 @@
+yay -Syu
+yay -Syu thorium-browser-bin thunar thunar-archive-plugin thunar-volman ark intel-opencl-sdk intel-opencl-runtime pip snapd google-chrome
+sudo pacman -Syu
+sudo pacman -Syu gstreamer mpv vlc jdk17-openjdk rsync git vlc wget flatpak qt5 cmake pocl clinfo glxinfo goverlay
+yay -Syu
+sudo pacman -Syu
+cd Descargas
+wget https://sdk.lunarg.com/sdk/download/1.3.275.0/linux/vulkansdk-linux-x86_64-1.3.275.0.tar.xz
+tar -xvzf /home/martinhhdz/Descargas/vulkansdk-linux-x86_64-1.3.275.0.tar.xz
+cd /home/martinhhdz/Descargas/1.3.275.0
+chmod +x vulkansdk
+./vulkansdk
+yay -Syu
+sudo pacman -Syu
