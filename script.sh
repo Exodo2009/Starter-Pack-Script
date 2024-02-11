5b9ff7aadfded88283155327f76f4e8297c3b253
Exodo2009 <135548519+Exodo2009@users.noreply.github.com>
2024-02-11 14:33:12 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-yay -S ark intel-opencl-sdk intel-opencl-runtime pip snapd google-chrome
-sudo pacman -S vlc jdk17-openjdk rsync git vlc wget flatpak qt5 cmake pocl clinfo glxinfo goverlay
+yay -Syu thunar thunar-archive-plugin thunar-volman ark intel-opencl-sdk intel-opencl-runtime pip snapd google-chrome
+sudo pacman -Syu vlc jdk17-openjdk rsync git vlc wget flatpak qt5 cmake pocl clinfo glxinfo goverlay
 cd Descargas
 wget https://sdk.lunarg.com/sdk/download/1.3.275.0/linux/vulkansdk-linux-x86_64-1.3.275.0.tar.xz
 tar -xvf /home/martinhhdz/Descargas/vulkansdk-linux-x86_64-1.3.275.0.tar.xz
